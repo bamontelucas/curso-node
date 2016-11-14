7759a16e272ea3747c15e4a04856e480f17d8401
Lucas Bamonte Corrêa <bamontelucas@gmail.com>
2016-11-14 11:33:51 -0200

    deixando igual do curso

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var app = require('./config/server');
 
+require('./routes/home')();
+require('./routes/formulario_inclusao_noticia')();
+require('./routes/noticias')();
+
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
--- config/server.js
+++ config/server.js
@@ -4,8 +4,4 @@ var config = require('./config');
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
-config.routes.forEach((r) => {
-    require(`${config.directories.routes}/${r}`)(app);
-});
-
 module.exports = app;
